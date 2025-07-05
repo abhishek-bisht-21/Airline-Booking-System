dfd302b0adf28a46f57ef34a60dc4511f87577c2
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2025-07-06 03:55:23 +0530

    Added Get ALL Airplanes API

--- FLIGHTS/src/routes/v1/airplane-route.js
+++ FLIGHTS/src/routes/v1/airplane-route.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const { AirplaneController } = require('../../controllers')
 const { AirplaneMiddlewares } = require('../../middleware/index')
 
-
+// /api/v1/airplanes POST
 router.post('/', AirplaneMiddlewares.validateCreateRequest, AirplaneController.createAirplane)
 
+// /api/v1/airplanes GET
+router.get('/', 
+    AirplaneController.getAirplanes);
 
 
 module.exports = router; 
