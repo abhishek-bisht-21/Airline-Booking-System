ba85ab860f5500f92f4a3a5315c71f7fc9b700a7
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2025-07-05 13:29:28 +0530

    Added basic crud repository

---
+++ FLIGHTS/src/repositories/crud-repository.js
@@ -0,0 +1,57 @@
+const {Logger} = require('../config/logger');
+
+class CrudRepository {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async create(data) {
+        try {   
+            const result = await this.model.create(data);
+            return result;
+        } catch (error) {
+            Logger.error("Something went wrong in the crud repository: create");
+            throw error;
+        }
+    }
+
+    async get(id) {
+        try {
+            const result = await this.model.findByPk(id);
+            return result;
+        } catch (error) {
+            Logger.error("Something went wrong in the crud repository: get");
+            throw error;
+        }
+    }
+
+    async destroy(data) {
+        try {
+            const result = await this.model.destroy({where: {id: data}});
+            return result;
+        } catch (error) {
+            Logger.error("Something went wrong in the crud repository: destroy");
+            throw error;
+        }
+    }
+
+    async update(id, data) {
+        try {
+            const result = await this.model.update(data, {where: {id: id}});
+            return result;
+        } catch (error) {
+            Logger.error("Something went wrong in the crud repository: update");
+            throw error;
+        }
+    }
+
+    async getAll() {
+        try {
+            const result = await this.model.findAll();
+            return result;
+        } catch (error) {
+            Logger.error("Something went wrong in the crud repository: getAll");
+            throw error;
+        }
+    }
+}
