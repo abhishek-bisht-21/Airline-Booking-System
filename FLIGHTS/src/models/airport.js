6a0f164d7a906c334e087d54bd6f3c87a084a005
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2025-07-06 16:21:19 +0530

    Added associations in city and airport model

--- FLIGHTS/src/models/airport.js
+++ FLIGHTS/src/models/airport.js
@@ -9,20 +9,20 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // static associate(models) {
-    //   // define association here
-    //   this.belongsTo(models.City, {
-    //     foreignKey: 'cityId',
-    //   });
-    //   this.hasMany(models.Flight, {
-    //     foreignKey: 'departureAirportId',
-    //     onDelete: 'CASCADE'
-    //   });
-    //   this.hasMany(models.Flight, {
-    //     foreignKey: 'arrivalAirportId',
-    //     onDelete: 'CASCADE'
-    //   });
-    // }
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.City, {
+        foreignKey: 'cityId',
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: 'departureAirportId',
+        onDelete: 'CASCADE'
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: 'arrivalAirportId',
+        onDelete: 'CASCADE'
+      });
+    }
   }
   Airport.init({
     name: {
